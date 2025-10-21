363828173748dd8ed32e286466566f30555a910e
Mukul Garg <mukulgarg0097@gmail.com>
2025-10-21 10:46:21 -0400

    correction in media-service

--- backend/media-service/media_service.py
+++ backend/media-service/media_service.py
@@ -1,9 +1,5 @@
 from app import create_app
 app = create_app()
 
-@app.route("/favicon.ico")
-def favicon():
-    return "", 204
-
 if __name__ == "__main__":
     app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
